dc107f7382b79cd07dd5759b6291f54ed404eef0
Ohnesorge <connero@iastate.edu>
2024-05-03 06:13:23 -0500

    sad

--- Data_memory.v
+++ Data_memory.v
@@ -10,18 +10,14 @@ module Data_memory (
     input MemtoReg,
     output reg [31:0] rData
 );
-
   parameter SIZE_DM = 128;  // size of this memory, by default 128*32
   reg [31:0] Dmem[SIZE_DM-1:0];  // instruction memory
-
-  // initially set default data to 0
   integer i;
   initial begin
     for (i = 0; i < SIZE_DM ; i = i + 1) begin
       Dmem[i] = 32'b0;
     end
   end
-
   always @(addr or MemRead or MemtoReg or ALUresult) begin
     if (MemRead == 1) begin
       if (MemtoReg == 1) begin
@@ -33,12 +29,10 @@ module Data_memory (
       rData = ALUresult;
     end
   end
-
   always @(posedge clk) begin  // MemWrite, wData, addr
     if (MemWrite == 1) begin
       Dmem[addr] = wData;
     end
   end
-
 endmodule
 
