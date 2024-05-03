55e99c4d2e7d93037f649c564ec14579f5b641a6
connero <88785126+conneroisu@users.noreply.github.com>
2024-05-03 04:02:05 -0500

    latest

--- fetch.v
+++ fetch.v
@@ -5,7 +5,6 @@ module fetch (
   input wire [31:0] pc;
   output wire [31:0] instruction;
   parameter NMEM = 15;  // Number of memory entries,
-  // not the same as the memory size
   parameter IM_DATA = "IM.mips";  // file to read data from // instruction memory 
   reg [31:0] mem[0:127];  // 32-bit memory with 128 entries
   initial begin
