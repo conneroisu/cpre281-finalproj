216a05513b153cb2b1fe6791df51606f9980d930
connero <88785126+conneroisu@users.noreply.github.com>
2024-05-03 07:09:58 -0500

    latest

--- Next_pc.v
+++ Next_pc.v
@@ -7,31 +7,31 @@ module Next_pc (
     input zero,
     output reg [31:0] next
 );
+
   reg [31:0] sign_ext;
   reg [31:0] old_alter;  // pc+4
   reg [31:0] jump;  // jump addr.
+  reg [31:0] jump_update;
   reg zero_alter;
+
   initial begin
     next = 32'b0;
   end
+
   always @(old) begin
     old_alter = old + 4;
   end
+
   always @(zero, Bne) begin
     zero_alter = zero;
     if (Bne == 1) begin
       zero_alter = !zero_alter;
     end
   end
-    always @(instruction) begin
-Error (10028): Can't resolve multiple constant drivers for net "jump[31]" at next_pc.v(26)
-Error (10029): Constant driver at next_pc.v(38)
-Error (10028): Can't resolve multiple constant drivers for net "jump[30]" at next_pc.v(26)
-Error (10028): Can't resolve multiple constant drivers for net "jump[29]" at next_pc.v(26)
 
   always @(instruction) begin
-    // jump-shift-left
-    jump = {4'b0, instruction[25:0], 2'b0};
+    // Calculate the jump address from the instruction
+    jump_update = {4'b0, instruction[25:0], 2'b0};
     // sign-extension
     if (instruction[15] == 1'b0) begin
       sign_ext = {16'b0, instruction[15:0]};
@@ -40,11 +40,16 @@ Error (10028): Can't resolve multiple constant drivers for net "jump[29]" at nex
     end
     sign_ext = {sign_ext[29:0], 2'b0};  // shift left
   end
+
   always @(posedge Jump or posedge old_alter) begin
+    // Update the jump register based on Jump signal
     if (Jump) begin
-      jump = {old_alter[31:28], jump[27:0]};
+      jump = {old_alter[31:28], jump_update[27:0]};
+    end else begin
+      jump = jump_update;
     end
   end
+
   always @(old_alter, sign_ext, jump, Branch, zero_alter, Jump) begin
     // assign next program counter value
     if (Branch == 1 && zero_alter == 1) begin
@@ -55,4 +60,5 @@ Error (10028): Can't resolve multiple constant drivers for net "jump[29]" at nex
       next = old_alter;
     end
   end
+
 endmodule
